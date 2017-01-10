b122a4d7db67a9fc3711c4fa3809e75abb72a6d6
Jérôme Mahuet <jerome.mahuet@gmail.com>
2017-01-10 01:24:34 +0100

    fuck boost fibers

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,6 @@
 
 using namespace std;
 
-
-void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
-    // When a user presses the escape key, we set the WindowShouldClose property to true,
-    // closing the application
-    // could also use
-    // glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GL_TRUE);
-    }
-}
-
 int main(int argc, char **argv) {
     Game game;
     game.pushState(std::make_unique<PlayState>(game));
